7ae24dd341d35405d995f97ec3d2e3e63fdf038f
Gokul <gokuljune2003@gmail.com>
2022-09-04 15:39:32 +0530

    added change

--- src/component/form.jsx
+++ src/component/form.jsx
@@ -136,7 +136,7 @@ function handlesumbit(event){
   <div className='container  '>
     
    <div className='container  shadow-lg  bg-body rounded' >
-   <div className=" mw-100 text-center" >
+   <div className=" mw-100 text-center" style={{width:'100%'}}>
        <span className="fs-5 text-dark font-monospace ">Expense Tracker</span>
        </div>
        <Selecttype style1 = {style.cbtn} style2={style.dbtn} function = {handlechange} />
