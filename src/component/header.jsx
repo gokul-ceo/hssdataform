91a7564754cea8998cb983e7ee3e107c2ab3f4be
Gokul <gokuljune2003@gmail.com>
2022-09-04 15:21:37 +0530

    added new changes

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Navbar(){
-    return  <div  className="container-sm  p-0 mx-auto text-center">
+    return  <div  className="container text-center">
       <span className="fs-4 fw-bold text-danger font-monospace">Sri Saravana</span>
     </div>
 }
