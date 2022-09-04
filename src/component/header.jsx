01436108a799f3ed190bd1bbb6287135e264a86c
Gokul <gokuljune2003@gmail.com>
2022-09-04 13:41:54 +0530

    testing

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Navbar(){
     return  <div  className="container-lg  p-0 mx-auto text-center">
-      <span className="fs-4 fw-bold text-danger font-monospace">Sri Saravana</span>
+      <span className="fs-4 fw-bold text-danger font-monospace"> Saravana</span>
     </div>
 }
 
