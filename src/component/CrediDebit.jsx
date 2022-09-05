43c9ada6e90b5bbc6ecb1d27482da7938f958f15
Gokul <gokuljune2003@gmail.com>
2022-09-05 12:20:07 +0530

    added new changes

--- src/component/CrediDebit.jsx
+++ src/component/CrediDebit.jsx
@@ -5,18 +5,18 @@ function CreditDebit(props){
         <div className='container-sm text-center my-5  display-data'>
        <div className='row gap-4'>
        <div className='col h-auto border rounded border-success'>
-         <span className='fs-6'style={props.style1}>Credit</span>
+         <span className='fs-1'style={props.style1}>Credit</span>
          <span>+{props.C}</span>
        </div>
        <div className='col h-auto border  rounded border-danger'>
-       <span className='fs-6' style={props.style2}>Debit</span>
+       <span className='fs-1' style={props.style2}>Debit</span>
        <span>-{props.D}</span>
        </div>
         </div>
         <div className='row'>
         <div className='col'>
           <div className='container-sm border bg-warning  rounded border-warning my-2'>
-            <span className='fs-6 my-auto'>Total: <span>{props.T}</span></span>
+            <span className='fs-1 my-auto'>Total: <span>{props.T}</span></span>
           </div>
         </div>
 
--- src/component/Selecttype.jsx
+++ src/component/Selecttype.jsx
@@ -6,10 +6,10 @@ function Selecttype(props){
         <div className='toggler-div mx-auto my-2 gap-3 container-sm text-center'>
        <div className='row'>
        <div className='col'>
-       <button type='button' style={props.style1} className='btn  btn-success ms-2 w-3 btn-lg' onClick={props.function} value='credit' >Credit</button>
+       <button type='button' style={props.style1} className='btn  btn-success ms-2 w-3 btn-lg' onClick={props.function} value='credit' >        Credit      </button>
        </div>
        <div className='col'>
-       <button type='button' style={props.style2} className='btn  btn-danger btn-lg' onClick={props.function} value='debit' >Debit</button>
+       <button type='button' style={props.style2} className='btn  btn-danger btn-lg' onClick={props.function} value='debit' >      Debit    </button>
        </div>
        </div>
        </div>
