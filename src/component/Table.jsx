ad62e8b691f81b4aff5cc1a9671eee8d40703979
Gokul <gokuljune2003@gmail.com>
2022-09-05 07:34:23 +0530

    added changes

--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -31,6 +31,7 @@ function Tabledisplay(){
 
   return <>
      <Navbar/>
+     <a href="/">click me!</a>
      <div className="container-md p-2">
       <div className="contianer-sm d-inline border-bottom border-5 border-info">
       <span className="fs-5 fw-bold text-info">Displaying of {TodayDate}</span>
@@ -56,6 +57,7 @@ function Tabledisplay(){
   </tbody>
   </table>
 </div>
+
   </>
     
   }
