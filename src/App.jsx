1f470cfecc1a2486adf96bc1107528a617f1daaf
Gokul <gokuljune2003@gmail.com>
2022-09-18 01:09:30 +0530

    testing files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Route, Routes} from 'react-router-dom';
-// import Tabledisplay from './component/Table'
-import Form from './component/form';
+// import React from 'react'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import { Route, Routes} from 'react-router-dom';
+// // import Tabledisplay from './component/Table'
+// import Form from './component/form';
 
-export function App(){
-  return (
-      <div>
-       <Routes>
-        <Route path='/' element={<Form/>}/>
-        {/* <Route path='/table' element={<Tabledisplay/>}/> */}
-       </Routes>
-      </div>
-  )
+// export function App(){
+//   return (
+//        <Routes>
+//         <Route path='/' element={<Form/>}/>
+//         {/* <Route path='/table' element={<Tabledisplay/>}/> */}
+//        </Routes>
+      
+//   )
+// }
+import React from "react";
+import {Billingkeyboard} from "./tescomponent/Billingkeyboard";
+import Header from "./tescomponent/Header";
+function App(){ 
+    return <>
+    <Header/>
+  
+    <Billingkeyboard/>
+    </>
 }
+
+export default App;
